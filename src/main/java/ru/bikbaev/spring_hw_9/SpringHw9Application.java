7d6a55bd616a9a4ccd2979b2d08f3b4084538eba
arturbikbaev <bigbyar4i@icloud.com>
2024-06-07 21:28:09 +0500

    hw_9

---
+++ src/main/java/ru/bikbaev/spring_hw_9/SpringHw9Application.java
@@ -0,0 +1,13 @@
+package ru.bikbaev.spring_hw_9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHw9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHw9Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bikbaev/spring_hw_9/SpringHw9ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bikbaev.spring_hw_9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHw9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
